c61fc3920296703454929c6be06b1ba1dc0fd092
daxiguaguagua-hash <daxiguaguagua@qq.com>
2026-04-03 17:28:21 +0800

    Constructor和Modifier，一个是初始化构造函数，一个是鉴权用函数。

---
+++ hello-web3/src/ConstructorModifierDemo.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ConstructorModifierDemo {
+    address public owner;
+    uint256 public number;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner, "Not the owner");
+        _onlyOwner();
+        _; // 原函数真正开始执行的位置
+    }
+
+    // internal 智能合约内部用
+    // view 只读函数，不修改状态变量
+    // 函数名前面加上“_”，表示这是一个辅助函数，社区内部约定俗成的命名规范
+    function _onlyOwner() internal view {
+        require(msg.sender == owner, "Not the owner");
+    }
+
+    function setNumber(uint256 _number) public onlyOwner {
+        number = _number;
+    }
+}
