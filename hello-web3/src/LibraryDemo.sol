57684692df28be8c9be7d613bcb2f9dfc7e1f1be
daxiguaguagua-hash <daxiguaguagua@qq.com>
2026-04-08 17:58:25 +0800

    import 也能够引入合约和interface

--- hello-web3/src/LibraryDemo.sol
+++ hello-web3/src/LibraryDemo.sol
@@ -1,10 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {MathLibrary, StringLibrary} from "./MathLibrary.sol";
+// import {MathLibrary, StringLibrary} from "./MathLibrary.sol"; // 最好是这样写
 // import "./MathLibrary.sol" as MathLib; // 也可以用 as 来给库取个别名，使用时就要 MathLib.max() 了。
+import "./MathLibrary.sol"; // 也可以这样写
+import {ICounter} from "./InterfaceDemo.sol"; // 也可以这样写
 
-contract LibraryDemo {
+// import contract 只是把“合约类型”引进来；真正使用它，还要通过继承、实例化或者地址绑定。`
+import "./Counter.sol"; // 也可以这样写
+
+contract LibraryDemo is ICounter {
+    function getNumber() external pure returns (uint256) {
+        return 42;
+    }
+
     function getMax(uint256 a, uint256 b) public pure returns (uint256) {
         return MathLibrary.max(a, b);
     }
